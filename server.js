acdec05f2c44d0ffafcd3ac1f6967dd31bc43576
Isabella <isabellapierce93@gmail.com>
2022-12-10 20:03:12 -0700

    Started server.js file

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'yourpassgoeshere',
+    database: 'employees_db'
+  },
+//   console.log(`Connected to the employees_db database.`)
+);
+
+db.connect(function (err){
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
+
+// Inquirer prompts
+
