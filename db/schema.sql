4b8ec505e68398e784ecc496b1cb7e2f0c5b3145
Isabella <isabellapierce93@gmail.com>
2023-01-02 16:42:25 -0700

    Updated Schema and Started working on prompts.

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NULL,
     salary DECIMAL(10.3) NULL,
     department_id INT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     roles_id INT NOT NULL,
-    manager_id INT NULL
+    manager_id INT NULL,
+    PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 // Dependencies
-const express = require('express');
+// const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: 'yourpassgoeshere',
+    password: '@ss$Dr@Y&sLzj9Yr',
     database: 'employees_db'
   },
-//   console.log(`Connected to the employees_db database.`)
+  console.log(`Connected to the employees_db database.`)
 );
 
 db.connect(function (err){
@@ -28,5 +28,52 @@ db.connect(function (err){
   }
 });
 
-// Inquirer prompts
+// Inquirer prompts for asking what action
+employeeApp = () => {
+  inquirer.prompt([
+    {
+      name: 'startMenu',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update Employee Role']
+    }
+  ]).then((response) => {
+      switch (response.startMenu) {
+        case 'View All Departments':
+          viewAllDepartments();
+          break;
+        case 'View All Roles':
+          viewAllRoles();
+          break;
+        case 'View All Employees':
+          viewAllEmployees();
+          break;
+        case 'Add A Department':
+          addADepartment();
+          break;
+        case 'Add A Role':
+          addARole();
+          break;
+        case 'Add An Employee':
+          addAnEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+      }
+  })
+};
 
+// View All Departments
+
+// View All Roles
+
+// View All Employees
+
+// Add A Department
+
+// Add A Role
+
+// Add an Employee
+
+// Update Employee Role
